71b7802f8e1a59c205f44f9190d962ca6fd73957
tobiasdickes <hanstodi2@gmail.com>
2022-01-02 13:28:46 +0100

    Fix Name Update | Make use of Calculated Permissions in DELETE_USER

--- src/main/java/de/tobias/spigotdash/web/sockets/SocketEventHandler.java
+++ src/main/java/de/tobias/spigotdash/web/sockets/SocketEventHandler.java
@@ -251,7 +251,7 @@ public class SocketEventHandler {
                         User u = main.UsersFile.getUserByName(name);
 
                         if(u != null) {
-                            if(!u.perms.USERS_IS_ADMIN) {
+                            if(!u.getCalculatedPerms().USERS_IS_ADMIN) {
                                 main.UsersFile.deleteUser(u);
                                 req.setResponse(200, "TEXT", "DELETED");
                             } else {
