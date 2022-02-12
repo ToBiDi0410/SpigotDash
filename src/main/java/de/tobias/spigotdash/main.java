bbed779ffd953ed10fe54712a229b88bd9e066f0
Tobias Dickes <hanstodi2@gmail.com>
2022-02-12 18:00:52 +0100

    Add Global Variables for Charset and GSON

--- src/main/java/de/tobias/spigotdash/main.java
+++ src/main/java/de/tobias/spigotdash/main.java
@@ -1,16 +1,20 @@
 package de.tobias.spigotdash;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import de.tobias.spigotdash.backend.logging.fieldLogger;
 import de.tobias.spigotdash.backend.logging.globalLogger;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class main extends JavaPlugin {
 
 	public static Plugin pl;
-
+	public static Charset GLOBAL_CHARSET = StandardCharsets.UTF_8;
+	public static Gson GLOBAL_GSON = new GsonBuilder().serializeNulls().setPrettyPrinting().disableHtmlEscaping().create();
 
 	public void onEnable() {
 
@@ -24,7 +28,6 @@ public class main extends JavaPlugin {
 
 			thisLogger.WARNING("It seems you have enabled the Debug Log!", 1);
 			thisLogger.WARNING("Please never share information from here to someone unknown!", 1);
-
 
 		} catch(Exception ex) {
 			ex.printStackTrace();
